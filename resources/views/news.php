4a1861f64e848aeecb6c7779392f1ffd7888a6b4
Roman <Melnikov_RV@list.ru>
2023-01-14 14:38:33 +0700

    dz-1

---
+++ resources/views/news.php
@@ -0,0 +1,21 @@
+<?php
+include_once "menu.php";
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Новости</title>
+</head>
+<body>
+<p>
+    Олимпийский чемпион по боксу Тищенко победил немца Манна на турнире в Екатеринбурге.
+</p>
+<p>
+    Боец Гасанов отметил, что самым сложным соперником для него является Токов.
+</p>
+</body>
+</html>
